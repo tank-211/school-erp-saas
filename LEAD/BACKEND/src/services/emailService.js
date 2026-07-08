9fe142a66d312df3eb38d473e73be374ed97d5a3
tank-211 <jadhavpratham1822@gmail.com>
2026-07-08 14:48:46 +0530

    Debug SMTP connection

--- LEAD/BACKEND/src/services/emailService.js
+++ LEAD/BACKEND/src/services/emailService.js
@@ -1,10 +1,13 @@
 import nodemailer from "nodemailer";
 
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
+  requireTLS: true,\
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
